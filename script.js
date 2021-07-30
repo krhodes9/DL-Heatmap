bee9a1d00c240c04dc6fa7a474acb4bbb5bc15b5
krhodes9 <61336730+krhodes9@users.noreply.github.com>
2021-07-30 14:25:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
-var map = L.map('map').setView([37.7, -122.4], 10);
+var map = L.map('map').setView([20.20, 17], 3);
 
 var OpenTopoMap = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.{ext}', {
 	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 	subdomains: 'abcd',
 	minZoom: 0,
-	maxZoom: 20,
+	maxZoom: 14,
 	ext: 'png'
 }).addTo(map);
 
-$.getJSON("https://raw.githubusercontent.com/gbrunner/adv-python-for-gis-and-rs/master/Week%201/sf_crime.geojson",function(data) {
+$.getJSON("https://opendata.arcgis.com/datasets/b3f84bff1c514484be7f4d65098f9372_0.geojson",function(data) {
   var coordinatesOnly = data.features.map(function(feature) {
     return [feature.geometry.coordinates[1], feature.geometry.coordinates[0], 1];
   });
 
   var heat = L.heatLayer(coordinatesOnly).addTo(map);
 });
+
+
+// make it so the heatmap becomes points at a certain zoom scale
+// hint: https://gis.stackexchange.com/questions/258515/show-hide-markers-depending-on-zoom-level
 
